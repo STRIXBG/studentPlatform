7882764fe23e34634774996931799aae96570acd
MacBook Pro <macbook@MacBooks-MacBook-Pro.local>
2023-11-24 10:53:02 +0200

    post pages created

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,27 +1,34 @@
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
   const [listOfPosts, setListOfPosts] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get("http://localhost:3001/posts").then((response) => {
       setListOfPosts(response.data);
     });
   }, []);
+
   return (
     <div>
-      {listOfPosts.map((value, key) => {
-        return (
-          <div className="post">
-            <div className="title"> {value.title} </div>
-            <div className="body">{value.postText}</div>
-            <div className="footer">{value.username}</div>
-          </div>
-        );
-      })}
+      {listOfPosts.map((value) => (
+        <div
+          className="post"
+          key={value.id}
+          onClick={() => {
+            navigate(`/post/${value.id}`);
+          }}
+        >
+          <div className="title"> {value.title} </div>
+          <div className="body">{value.postText}</div>
+          <div className="footer">{value.username}</div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
