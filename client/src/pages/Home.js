f34d8fd4e4dfa0d4baf94419a96a8dcf85158dda
MacBook Pro <macbook@MacBooks-MacBook-Pro.local>
2023-11-24 19:32:24 +0200

    like system

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,19 +12,56 @@ function Home() {
     });
   }, []);
 
+  const likeAPost = (postId) => {
+    axios
+      .post(
+        "http://localhost:3001/likes",
+        { PostId: postId },
+        { headers: { accessToken: localStorage.getItem("accessToken") } }
+      )
+      .then((response) => {
+        setListOfPosts((prevPosts) =>
+          prevPosts.map((post) => {
+            if (post.id === postId) {
+              if (response.data.liked) {
+                return { ...post, Likes: [...post.Likes, 0] };
+              } else {
+                const likesArray = [...post.Likes];
+                likesArray.pop();
+                return { ...post, Likes: likesArray };
+              }
+            }
+            return post;
+          })
+        );
+      });
+  };
+
   return (
     <div>
-      {listOfPosts.map((value) => (
-        <div
-          className="post"
-          key={value.id}
-          onClick={() => {
-            navigate(`/post/${value.id}`);
-          }}
-        >
+      {listOfPosts.map((value, key) => (
+        <div key={key} className="post">
           <div className="title"> {value.title} </div>
-          <div className="body">{value.postText}</div>
-          <div className="footer">{value.username}</div>
+          <div
+            className="body"
+            onClick={() => {
+              navigate(`/post/${value.id}`);
+            }}
+          >
+            {value.postText}
+          </div>
+          <div className="footer">
+            {value.username}{" "}
+            <button
+              onClick={() => {
+                likeAPost(value.id);
+              }}
+            >
+              {" "}
+              Like
+            </button>
+            <label> {value.Likes.length}</label>
+          </div>
         </div>
       ))}
     </div>
