009e3d2e7554d75f5d919d5383e9cc1774ec18c0
MacBook Pro <macbook@MacBooks-MacBook-Pro.local>
2023-11-24 10:08:12 +0200

    configure cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+// Middleware to set CORS headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Allow all origins (you can specify a specific origin instead)
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Add allowed methods
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Add allowed headers
+  next();
+});
+
 app.use(express.json());
 
 const db = require("./models");
